106dbb4999fe0757ba9566c959d5b0b58cce957d
Javier Rodríguez <frodsot2402@g.educaand.es>
2024-10-16 10:12:23 +0200

    Prototipo

---
+++ src/app/cabecera/cabecera.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'cabecera',
+  standalone: true,
+  imports: [],
+  templateUrl: './cabecera.component.html',
+  styleUrl: './cabecera.component.css'
+})
+export class CabeceraComponent {
+
+  @Input() srcImagen!: String
+
+}
